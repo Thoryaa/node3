7af43272ca5a509138f48b516f6018238f4fd11f
Thoryaa <93941131+Thoryaa@users.noreply.github.com>
2022-02-18 14:09:37 +0300

    support styling

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.listen(2000);
+app.set('view engine', 'ejs');
+
+app.use(express.static('views/assests'));
+
+console.log('yes');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        par: "Iam webdeveloper I STUDIED at Taiz univeristyworked at lamas company, lamas company, .I am herefor you to give you advice and to do your web site Iam webdeveloper TUDIED at Taiz univeristy worked at lamas company   lamas compan I am here "
+    });
+})
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header('loccation:/views/index.ejs');
+?>
