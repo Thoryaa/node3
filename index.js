39b844ffc5ee7d166b9d9b1cc7a6725e18178b48
Thoryaa <93941131+Thoryaa@users.noreply.github.com>
2022-02-18 18:19:35 +0300

    server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+
+app.listen(process.env.PORT || 3000);;
 app.set('view engine', 'ejs');
 
 app.use(express.static('views/assests'));
